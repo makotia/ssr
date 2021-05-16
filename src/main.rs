29b9743a5a2d791550797cb485d40d0c6a056ecd
makotia <3mdevmail@gmail.com>
2021-05-17 08:01:25 +0900

    Split to lib (#3)
    * split to lib
    
    * fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::env;
+use std::{env, fs};
 
-extern crate image;
-use image::imageops;
+use ssr::crop_image;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let img_path = match args.first() {
+    let input_path = match args.first() {
         None => {
             eprintln!("Error: Input file path is not specified. ");
             return;
@@ -14,23 +13,12 @@ fn main() {
         Some(s) => s,
     };
 
-    let img = image::open(&img_path).unwrap();
-    let mut img = img.to_rgb8();
-
-    let input_height = img.height();
-    let input_width = img.width();
-    let height = input_width / 16 * 9;
-
-    let res = imageops::crop(
-        &mut img,
-        0,
-        (input_height - height) / 2,
-        input_width,
-        height,
-    );
-
-    match res.to_image().save(img_path) {
-        Ok(_) => println!("Succeed."),
-        Err(why) => eprintln!("Error!: {:?}", why),
+    match fs::read_dir(input_path) {
+        Ok(v) => {
+            for p in v {
+                crop_image(p.unwrap().path().to_str().unwrap());
+            }
+        }
+        Err(_) => crop_image(input_path),
     };
 }
